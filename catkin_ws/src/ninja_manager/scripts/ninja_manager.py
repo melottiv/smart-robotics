f78f95766d3c0923ae1b44fbb13c268e7a50e531
melottiv <virginiamelotti@gmail.com>
2025-07-24 17:51:03 +0200

    modelli 3D facili

--- catkin_ws/src/ninja_manager/scripts/ninja_manager.py
+++ catkin_ws/src/ninja_manager/scripts/ninja_manager.py
@@ -19,45 +19,34 @@ path = rospkg.RosPack().get_path("ninja_manager")
 # DEFAULT PARAMETERS
 package_name = "ninja_manager"
 spawn_name = '_spawn'
-level = None
-selectBrick = None
-maxLego = 11
-spawn_pos = (-0.38, -0.35, 0.74)  # center of spawn area
-spawn_dim = (0.25, 0.2)  # spawning area
-min_space = 0.010  # min space between screws and nuts
-min_distance = 0.15  # min distance between screws and nuts
+spawn_pos = (-0.38, -0.35, 0.74)
+spawn_dim = (0.25, 0.2)
+min_space = 0.010
+min_distance = 0.15
 
-screwDict = {
-    'screw_25x8': (0, (0.0277128, 0.06, 0.024)),
-    'nut_6x9': (1, (0.0277128, 0.024, 0.012))
+ingredientDict = {
+    'bread':    (0, (0.08, 0.08, 0.025)),      # cilindro: diametro 8cm
+    'tomato':   (1, (0.07, 0.07, 0.01)),      # cilindro: diametro 7cm
+    'cheese':   (2, (0.075, 0.075, 0.003)),    # cubo 7.5cm x 7.5cm x 0.3cm
+    'meat':     (3, (0.075, 0.075, 0.02)),     # cilindro: diametro 7.5cm
+    'salad':    (4, (0.07, 0.07, 0.005))       # cilindro: diametro 7cm
 }
 
-# color bricks
-colorList = ['Gazebo/Indigo', 'Gazebo/Orange',
-             'Gazebo/Red', 'Gazebo/Purple',
-             'Gazebo/DarkYellow', 'Gazebo/Green']
 
-screwList = list(screwDict.keys())
-counters = [0 for screw in screwList]
-
-spawned_screws = []  # screw_or_nut = [[name, type, pose, radius], ...]
-
-
-def change_model_color(model_xml, color):
-    root = ET.XML(model_xml)
-    root.find('.//material/script/name').text = color
-    return ET.tostring(root, encoding='unicode')
+ingredientList = list(ingredientDict.keys())
+counters = [0 for _ in ingredientList]
+spawned_ingredients = []
 
 
 # get model path
 def get_model_path(model):
     pkg_path = rospkg.RosPack().get_path(package_name)
-    return f'{pkg_path}/screws_and_nuts_models/{model}/model.sdf'
+    return f'{pkg_path}/ingredients_models/{model}/model.sdf'
 
 
-# set position brick
-def random_pose(screw_type):
-    _, dim, = screwDict[screw_type]
+# set position 
+def random_pose(model_type):
+    _, dim = ingredientDict[model_type]
     spawn_x = spawn_dim[0]
     spawn_y = spawn_dim[1]
     rot_x = 0
@@ -66,12 +55,10 @@ def random_pose(screw_type):
     point_x = random.uniform(-spawn_x, spawn_x)
     point_y = random.uniform(-spawn_y, spawn_y)
     point_z = dim[2] / 2
-    screw_dimx = dim[0]
-    screw_dimy = dim[1]
 
     rot = Quaternion(*quaternion_from_euler(rot_x, rot_y, rot_z))
     point = Point(point_x, point_y, point_z)
-    return Pose(point, rot), screw_dimx, screw_dimy
+    return Pose(point, rot), dim[0], dim[1]
 
 
 class PoseError(Exception):
@@ -79,20 +66,18 @@ class PoseError(Exception):
 
 
 # function to get a valid pose
-def get_valid_pose(screwtype):
+def get_valid_pose(model_type):
     trys = 1000
     valid = False
-    pos = None
-    radius = None
     while not valid:
-        pos, dim1, dim2 = random_pose(screwtype)
-        radius = np.sqrt((dim1 ** 2 + dim2 ** 2)) / 2
+        pos, dim1, dim2 = random_pose(model_type)
+        radius = np.sqrt((dim1**2 + dim2**2)) / 2
         valid = True
-        for screw in spawned_screws:
-            point = screw[2].position
-            r2 = screw[3]
+        for ing in spawned_ingredients:
+            point = ing[2].position
+            r2 = ing[3]
             min_dist = max(radius + r2 + min_space, min_distance)
-            if (point.x - pos.position.x) ** 2 + (point.y - pos.position.y) ** 2 < min_dist ** 2:
+            if (point.x - pos.position.x)**2 + (point.y - pos.position.y)**2 < min_dist**2:
                 valid = False
                 trys -= 1
                 if trys == 0:
@@ -102,100 +87,65 @@ def get_valid_pose(screwtype):
 
 
 # function to spawn model
-def spawn_model(model, pos, name=None, ref_frame='world', color=None):
+def spawn_model(model, pos, name=None, ref_frame='world'):
     if name is None:
         name = model
-
     model_xml = open(get_model_path(model), 'r').read()
-    if color is not None:
-        model_xml = change_model_color(model_xml, color)
-
     spawn_model_client = rospy.ServiceProxy('/gazebo/spawn_sdf_model', SpawnModel)
     return spawn_model_client(model_name=name,
                               model_xml=model_xml,
-                              robot_namespace='/foo',
+                              robot_namespace='/food',
                               initial_pose=pos,
                               reference_frame=ref_frame)
 
 
-# support function delete bricks on table
-def delete_model(name):
-    delete_model_client = rospy.ServiceProxy('/gazebo/delete_model', DeleteModel)
-    return delete_model_client(model_name=name)
+
+# Lista ordinata degli ingredienti da spawnare ogni volta
+fixed_ingredient_order = ['bread', 'meat', 'cheese', 'salad', 'tomato','bread']
 
 
-# support functon spawn bricks
-def spawn_screw():
-    screw_type = random.choice(screwList)
+def spawn_all_ingredients():
+    for model_type in fixed_ingredient_order:
+        idx = ingredientDict[model_type][0]
+        name = f'{model_type}_{str(time.time_ns())[-9:]}'
+        pos, radius = get_valid_pose(model_type)
+        spawn_model(model_type, pos, name, spawn_name)
+        spawned_ingredients.append((name, model_type, pos, radius))
+        counters[idx] += 1
 
-    screw_idx = screwDict[screw_type][0]
-    name = f'{screw_type}_{str(time.time_ns())[-9:]}'
-    pos, radius = get_valid_pose(screw_type)
-    assert pos is not None, "pos is None"
-    assert radius is not None, "radius is None"
-    color = random.choice(colorList)
-
-    spawn_model(screw_type, pos, name, spawn_name, color)
-    spawned_screws.append((name, screw_type, pos, radius))
-    counters[screw_idx] += 1
 
 
 # main function setup area and level manager
-def set_up_area(screws_number=None):
-    rospy.init_node('element_spawner')
+def set_up_area():
+    rospy.init_node('ingredient_spawner')
 
-    # delete all nuts and screws on the table
-    models = rospy.wait_for_message("/gazebo/model_states", ModelStates, timeout=None)
+    models = rospy.wait_for_message("/gazebo/model_states", ModelStates)
     environment_elements = ["ground_plane", "modern_table", "kinect", "robot", "_spawn"]
-    spawn_area_exists = False
+    spawn_area_exists = any(m.startswith('_spawn') for m in models.name)
+
     for model in models.name:
-        if model.startswith('_spawn'):
-            spawn_area_exists = True
-        if model not in environment_elements and (model.startswith('screw') or model.startswith('nut')):
+        if model not in environment_elements and any(model.startswith(i) for i in ingredientList):
             delete_model(model)
 
     if not spawn_area_exists:
-        spawn_model(spawn_name, Pose(Point(*spawn_pos), None))
+        spawn_model(spawn_name, Pose(Point(*spawn_pos), Quaternion(0, 0, 0, 1)))
 
     try:
-        for _ in range(screws_number):
-            spawn_screw()
-    except PoseError as _:
+        spawn_all_ingredients()   # <-- qui 4 spazi di indentazione
+    except PoseError:
         print("[Error]: no space in spawning area")
-        pass
 
-    print(f"Added {len(spawned_screws)} screw(s) or nut(s)")
+    print(f"Added {len(spawned_ingredients)} ingredient(s)")
 
 
 if __name__ == '__main__':
 
-    parser = argparse.ArgumentParser(description='Spawn a specified number of screws.')
-    parser.add_argument('-n', '--number', type=int, required=True, help='Number of screws to spawn')
-
-    args = parser.parse_args()
-    number_of_screws = args.number
-
     try:
         if '/gazebo/spawn_sdf_model' not in rosservice.get_service_list():
-            print("Waining gazebo service..")
+            print("Waiting for gazebo service..")
             rospy.wait_for_service('/gazebo/spawn_sdf_model')
-
-        spawn_model(spawn_name, Pose(Point(*spawn_pos), None))
-        set_up_area(number_of_screws)
+        spawn_model(spawn_name, Pose(Point(*spawn_pos), Quaternion(0, 0, 0, 1)))
+        set_up_area()
         print("All done. Ready to start.")
-    except rosservice.ROSServiceIOException as err:
-        print("No ROS master execution")
-        pass
-    except rospy.ROSInterruptException as err:
-        print(err)
-        pass
-    except rospy.service.ServiceException:
-        print("No Gazebo services in execution")
-        pass
-    except rospkg.common.ResourceNotFound:
-        print(f"Package not found: '{package_name}'")
-        pass
-    except FileNotFoundError as err:
-        print(f"Model not found: \n{err}")
-        print(f"Check model in folder'{package_name}/lego_models'")
-        pass
+    except Exception as e:
+        print(f"Exception: {e}")
